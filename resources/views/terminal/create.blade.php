16ce35286295a74dfbf1cb787fd86665309e14ae
Simon Pangan <simonjoseph.pangan@gmail.com>
2024-02-07 21:21:12 +0800

    finish

--- resources/views/terminal/create.blade.php
+++ resources/views/terminal/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="container mt-4">
-        <h2>Create Terminal</h2>
+        <h2 class="text-center">Create Terminal</h2>
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -11,34 +11,37 @@
             </div>
         @endif
 
-        <form method="post" action="{{ route('admin.terminals.store') }}" enctype="multipart/form-data">
-            @csrf
+        <div class="mx-auto" style="max-width: 500px">
+            <form method="post" action="{{ route('admin.terminals.store') }}" enctype="multipart/form-data">
+                @csrf
 
-            <div class="mb-3">
-                <label for="from" class="form-label">From</label>
-                <input type="text"
-                       class="form-control @error('from') is-invalid @enderror" id="name" name="from" value="{{ old('from') }}" required>
-                @error('from')
-                <span class="invalid-feedback" role="alert">
+                <div class="mb-3">
+                    <label for="from" class="form-label">From</label>
+                    <input type="text"
+                           class="form-control @error('from') is-invalid @enderror" id="name" name="from"
+                           value="{{ old('from') }}" required>
+                    @error('from')
+                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-3">
-                <label for="to" class="form-label">To</label>
-                <input type="text" class="form-control @error('to') is-invalid @enderror"
-                       id="to" name="to" value="{{ old('to') }}" required>
+                <div class="mb-3">
+                    <label for="to" class="form-label">To</label>
+                    <input type="text" class="form-control @error('to') is-invalid @enderror"
+                           id="to" name="to" value="{{ old('to') }}" required>
 
-                @error('to')
-                <span class="invalid-feedback" role="alert">
+                    @error('to')
+                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <button type="submit" class="btn btn-primary">Create Terminal</button>
-        </form>
+                <button type="submit" class="btn btn-primary">Create Terminal</button>
+            </form>
+        </>
     </div>
 
 </x-app-layout>
--- resources/views/terminal/edit.blade.php
+++ resources/views/terminal/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="container mt-4">
-        <h2>Create Terminal</h2>
+        <h2 class="text-center">Edit Terminal</h2>
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -11,35 +11,37 @@
             </div>
         @endif
 
-        <form method="post" action="{{ route('admin.terminals.update', $terminal) }}" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="mb-3">
-                <label for="from" class="form-label">From</label>
-                <input type="text"
-                       class="form-control @error('from') is-invalid @enderror" id="name" name="from"
-                       value="{{ old('from', $terminal->from) }}" required>
-                @error('from')
-                <span class="invalid-feedback" role="alert">
+        <div class="mx-auto" style="max-width: 500px">
+            <form method="post" action="{{ route('admin.terminals.update', $terminal) }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <label for="from" class="form-label">From</label>
+                    <input type="text"
+                           class="form-control @error('from') is-invalid @enderror" id="name" name="from"
+                           value="{{ old('from', $terminal->from) }}" required>
+                    @error('from')
+                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <div class="mb-3">
-                <label for="to" class="form-label">To</label>
-                <input type="text" class="form-control @error('to') is-invalid @enderror"
-                       id="to" name="to" value="{{ old('to', $terminal->to) }}" required>
+                <div class="mb-3">
+                    <label for="to" class="form-label">To</label>
+                    <input type="text" class="form-control @error('to') is-invalid @enderror"
+                           id="to" name="to" value="{{ old('to', $terminal->to) }}" required>
 
-                @error('to')
-                <span class="invalid-feedback" role="alert">
+                    @error('to')
+                    <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                @enderror
-            </div>
+                    @enderror
+                </div>
 
-            <button type="submit" class="btn btn-primary">Create Terminal</button>
-        </form>
+                <button type="submit" class="btn btn-primary">Edit Terminal</button>
+            </form>
+        </div>
     </div>
 
 </x-app-layout>
