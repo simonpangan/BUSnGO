e0bbc022160ebd2a7888214d940f4c7347cc432e
Simon Pangan <simon.pangan@elitesdsi.com>
2024-03-13 12:57:34 +0800

    update

--- app/Http/Controllers/BusStatusUpdateController.php
+++ app/Http/Controllers/BusStatusUpdateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
 use App\Models\Schedule;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,15 @@ class BusStatusUpdateController extends Controller
     public function __invoke(Request $request)
     {
         $schedule = Schedule::findOrFail($request->schedule_id);
+
+        if ($request->status === 'Onboarding') {
+            $payments = Payment::where('schedule_id', $request->schedule_id)->get();
+            if (count($payments) > 0) {
+                dd($payments);
+                dd($payments[0]->passenger);
+            }
+        }
+
         $schedule->update([
             'status' => $request->status
         ]);
