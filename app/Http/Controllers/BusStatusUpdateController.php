b926fe614f49f0946fb2e284101265bee73f2f12
Simon Pangan <simon.pangan@elitesdsi.com>
2024-03-14 12:49:53 +0800

    update

--- app/Http/Controllers/BusStatusUpdateController.php
+++ app/Http/Controllers/BusStatusUpdateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Payment;
 use App\Models\Schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class BusStatusUpdateController extends Controller
 {
@@ -14,9 +15,19 @@ class BusStatusUpdateController extends Controller
 
         if ($request->status === 'Onboarding') {
             $payments = Payment::where('schedule_id', $request->schedule_id)->get();
+            //There is no payments sending m360 is a waste
             if (count($payments) > 0) {
-                dd($payments);
-                dd($payments[0]->passenger);
+                foreach ($payments as $payment) {
+                    Http::post('https://api.m360.com.ph/v3/api/broadcast', [
+                        "username" => "Project_case",
+                        "password" => "SScDpn6J",
+                        "msisdn" => $payment->contact_no,
+                        "content" => "Hello,this is a samplebroadcast.",
+                        "shortcode_mask" => "BUSnGO",
+                        "is_intl" => false
+                    ]);
+                }
+                dd('asd');
             }
         }
 
